c85a5b5e403c70addfede5bbdbeb124ba1da2ed2
Kyle Partridge <partkyle@gmail.com>
2024-02-01 12:59:06 -0800

    Update extension.ts
    remove some erroneous comments and commented out code

--- src/extension.ts
+++ src/extension.ts
@@ -81,51 +81,14 @@ let paste_and_indent = () => {
 				}
 			});
 		});
-	
-
-		// .then(success => {
-		// 	myedit.selections.forEach(selection => {
-		// 		let postion = selection.end;
-		// 		selection = new vscode.Selection(postion, postion);
-		// 	});
-		// });
-
-		// myedit.selections.forEach(selection => {
-		// 	myedit.edit((editBuilder: vscode.TextEditorEdit) => {
-		// 		editBuilder.replace(selection, "asdfasdfa");
-		// 	}).then(success => {
-		// 		if (success) {
-		// 			console.log('thig thing alkjsdflkjasdflkj');
-		// 			// make selection empty
-		// 			var postion = myedit.selection.end; 
-		// 			selection = new vscode.Selection(postion, postion);
-		// 		}
-		// 	});
-		// });
 	}
 };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	let indent = vscode.commands.registerCommand('extension.paste-and-indent', paste_and_indent);
 	context.subscriptions.push(indent);
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World!');
-	// });
-
-	// context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
